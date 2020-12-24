a955dc42aa1bf05d01ebc5acbe313c81ab0f418d
Vishesh Singh <VisheshSingh16@gmail.com>
2020-12-23 22:14:21 -0600

    first commit

---
+++ shopping.js
@@ -0,0 +1,34 @@
+const shoppingForm = document.querySelector('.shopping');
+const list = document.querySelector('.list');
+
+const items = [];
+
+function handleSubmit(e) {
+  e.preventDefault();
+  const name = e.currentTarget.item.value;
+  if (!name) {
+    return;
+  }
+  const item = {
+    name,
+    id: Date.now(),
+    checked: false,
+  };
+  items.push(item);
+  e.target.reset();
+  console.log(items);
+  displayItems();
+}
+
+function displayItems() {
+  const html = items.map((item) => {
+    return `<li class='shopping-item'>
+            <input type='checkbox' />
+            <span class='itemName'>${item.name}</span>
+            <button aria-label='Remove ${item.name}'>&times;</button>
+        </li>`;
+  });
+  list.innerHTML = html.join('');
+}
+
+shoppingForm.addEventListener('submit', handleSubmit);
